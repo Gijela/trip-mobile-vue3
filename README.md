e38c545c12a56cbc13f5b654411c015a8656610e
jsdevin <jsdevin@163.com>
2022-10-02 22:17:49 +0800

    完成项目：弘源旅途

---
+++ README.md
@@ -0,0 +1,2 @@
+# 弘源旅途
+开发过程[点击这里](https://devin.ren/project/trip-mobile-vue3/01-%E9%A6%96%E9%A1%B5tabbar%E3%80%81navBar%E3%80%81banner%E3%80%81location.html#navbar%E3%80%81banner%E3%80%81location)查看
---
+++ src/views/favor/favor.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="favor" ref="favorRef">
+    <h2>未完待续</h2>
+    <h3>
+      详细开发思路请
+      <a
+        href="https://devin.ren/project/trip-mobile-vue3/01-%E9%A6%96%E9%A1%B5tabbar%E3%80%81navBar%E3%80%81banner%E3%80%81location.html#navbar%E3%80%81banner%E3%80%81location"
+        style="font-color: blue"
+        >点击这里</a
+      >
+      查看
+    </h3>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import useScroll from "@/hooks/useScroll";
+
+const favorRef = ref();
+
+useScroll(favorRef);
+</script>
+
+<style lang="less" scoped></style>
